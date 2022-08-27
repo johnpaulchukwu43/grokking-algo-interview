fbb791cb8d0506ce68eeb0a3d2e5d7cda6701a4e
jchukwu <jchukwu@teamapt.com>
2022-08-27 23:44:29 +0100

    WIP MergeIntervals

---
+++ src/com/jworks/patterns/mergedintervals/Interval.java
@@ -0,0 +1,11 @@
+package com.jworks.patterns.mergedintervals;
+
+class Interval {
+  int start;
+  int end;
+
+  public Interval(int start, int end) {
+    this.start = start;
+    this.end = end;
+  }
+};
